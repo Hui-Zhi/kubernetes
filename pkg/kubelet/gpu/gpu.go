1d3d124bebedd356c2b9f96ae130fe66338c11e0
Hui-Zhi <zhaohuizhi@gmail.com>
2016-05-28 15:32:50 -0400

    changes in gpu.go

--- pkg/kubelet/gpu/gpu.go
+++ pkg/kubelet/gpu/gpu.go
@@ -1,15 +1,13 @@
 package gpu
 
 import (
-//	"github.com/golang/glog"
-//	"k8s.io/kubernetes/pkg/api"
-//	"k8s.io/kubernetes/pkg/kubelet/gpu/nvidia"
+	//	"github.com/golang/glog"
+	//	"k8s.io/kubernetes/pkg/api"
+	//	"k8s.io/kubernetes/pkg/kubelet/gpu/nvidia"
 	gpuTypes "k8s.io/kubernetes/pkg/kubelet/gpu/types"
 )
-
 
 func ProbeGPUPlugins() []gpuTypes.GPUPlugin {
 	allPlugins := []gpuTypes.GPUPlugin{}
 	return allPlugins
 }
-
